468ec8d1e2d25bd9f3d8b1b4a7a399bd4bd46b1a
sahil-dev-eloper <sahildargar@gmail.com>
2025-08-20 22:01:23 +0530

    authcontroller modified

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,9 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
 import User from "../models/User.js";
-
-dotenv.config();
 
 // Register a new user
 export const registerUser = async (req, res) => {
